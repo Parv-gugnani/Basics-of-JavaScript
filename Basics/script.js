eb314e50eb61ddb2857034491ad5ffa7b03c7382
Parv-gugnani <9765.parv@gmail.com>
2023-04-17 18:47:16 +0530

    Basic-1

---
+++ Basics/script.js
@@ -0,0 +1,95 @@
+"use strict";
+
+/*
+// definations
+ "/* this is for the whole code"
+
+*/
+
+/*
+//printing ---
+console.log("Hello World"); //printing
+
+//popping hello world via promt
+// alert("Hello World");
+
+// document.write("Hello world");
+
+*/
+
+/*
+//Playing with two number
+
+const a = 3;
+const b = 2;
+
+const adding = a + b;
+const multiplie = a * b;
+const dividing = a / b;
+
+console.log(adding);
+console.log(multiplie);
+console.log(dividing);
+*/
+
+/*
+Playing with number entered by user --->
+
+// const num1 = parseInt(prompt(`First number`));
+// const num2 = parseInt(prompt(`second number`));
+
+prase int 
+this function converts the userinput string 
+into integer
+
+for example : ("t007")
+it wont convert as the first elemnt is string
+it only conver string number into integer 
+
+
+const sum2 = num1 + num2;
+const multiplie2 = num1 * num2;
+
+// `` we use this template literal to include varible inside
+//${variable} it is like %d in c programing, take data from above stored variable
+console.log(`Sum of ${num1} and ${num2} is = ${sum2}`);
+console.log(`Multiplication of ${num1} and ${num2} is = ${multiplie2}`);
+*/
+
+/*
+// square root = *
+
+//maths.sqrt(number);//take a number and return it as a square root
+
+const num1 = prompt("Enter The number:");
+const result = Math.sqrt(num1);
+console.log(`The square root of ${num1} is ${result}`);
+*/
+
+/*
+//swap two variable
+let a = prompt("Enter The first one");
+let b = prompt("Enter The second one");
+
+let temp;
+
+//swaaping
+temp = a;
+a = b;
+b = temp;
+or we can array : [a,b] = [b,a];//work same as it did
+
+
+console.log(`The value of a after swapping ${a}`);
+console.log(`The value of b after swapping ${b}`);
+*/
+
+/*
+//conversion
+const km = prompt("Enter the value of km");
+
+const fac = 0.621371;
+
+const mil = km * fac;
+console.log(`${km} kilometers is equal to ${mil} Miles`);
+*/
