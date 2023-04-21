f675795bd855df35139a3c997d9756367e74e85d
Parv-gugnani <9765.parv@gmail.com>
2023-04-21 12:29:18 +0530

    colorflicker

--- Color Flicker/script.js
+++ Color Flicker/script.js
@@ -4,10 +4,12 @@ let colors = ["red", "black", "purple", "yellow", "green", "blue", "orange"];
 
 let button = document.getElementById("button");
 
+/* getelement by id we dont have to specify that it is id */
+
 button.addEventListener("click", function () {
   let index = parseInt(Math.random() * colors.length + 1); //parseint will return integer
 
   let canvas = document.getElementById("canvas");
 
-  canvas.style.background = `${colors[index]}`;
+  canvas.style.background = `${colors[index]}`; //taking colors
 });
